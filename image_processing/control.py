34537341c50007ca57a434b601aff76e1bc944f4
bmangust <ftf.pollux@gmail.com>
2020-03-07 18:53:38 +0300

    camera controled motor

--- image_processing/control.py
+++ image_processing/control.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
-import time
+from time import sleep, time
 from picamera import PiCamera
+from PIL import Image, ImageStat
+import imageio
 
 GPIO.setmode(GPIO.BCM)
 buttons = [20, 21, 14, 6]
@@ -117,12 +119,16 @@ motor_commands = {
     'forward': fullstep_fw,
     'backward': fullstep_bw}
 
-camera = PiCamera()
+current_position = 0
+start_position = 0
+end_position = 255
 
+camera = PiCamera()
+camera.resolution = (64, 64)
 
 def pulsePin(pin):
     GPIO.output(pin, 1)
-    time.sleep(0.0001)
+    sleep(0.0001)
     GPIO.output(pin, 0)
     
 
@@ -130,21 +136,52 @@ def outputCommand(cmd):
     for bit in cmd:
         GPIO.output(dataPin, bit)
         pulsePin(clockPin)
-        time.sleep(0.00001)
+        sleep(0.00001)
     pulsePin(latchPin)
 
 
-def rotate(selector):
+def rotateOneStep( direction ):
+    global current_position
+    current_position = 0
+    if direction > 0:
+        selector = 'forward1'
+    else:
+        selector = 'backward1'
+    for i in range(5):
+        for cmd in motor_commands[selector]:
+            outputCommand(cmd)
+        
+
+
+def rotateToPosition( position ):
+    global current_position
     cnt = 0
-    print(selector)
-    while cnt < 280:
+    steps = position - current_position
+    if steps > -5 and steps < 5:
+        return
+    if steps <= -5:
+        selector = 'forward1'
+    elif steps >= 5:
+        selector = 'backward1'
+    steps = abs( steps )
+#     print( selector )
+    while cnt < steps:
         cnt += 1
         for cmd in motor_commands[selector]:
             outputCommand(cmd)
+    current_position = position
+
+# def rotate( selector ):
+#     cnt = 0
+#     print(selector)
+#     while cnt < 280:
+#         cnt += 1
+#         for cmd in motor_commands[selector]:
+#             outputCommand(cmd)
 
 
 def readButtons():
-    time.sleep(0.2)
+    sleep(0.2)
     btns = [
         GPIO.input(buttons[0]),
         GPIO.input(buttons[1]),
@@ -155,31 +192,70 @@ def readButtons():
 
 
 def takePicture():
+#     camera.image_effect = 'solarize'
     camera.capture('/home/pi/Desktop/image.jpg')
-#     time.sleep(1)
-    print('picture_taken')
+#     print('picture_taken')
 
+
+def isDark(img):
+    sum = 0
+    for i in range(len(img)):
+        row = []
+        for j in range(len(img[i])):
+            s = 0;
+            for k in range(len(img[i][j])):
+                s += img[i][j][k]
+            s //= 3
+            sum += s
+    sum //= len(img) * len(img[0])
+    print (sum)
+
+
+def brightness( im_file ):
+    im = Image.open(im_file).convert('L')
+    stat = ImageStat.Stat(im)
+    return stat.mean[0]
+
+
+def processImage():
+    global current_position
+    file = '/home/pi/Desktop/image.jpg'
+    while ( True ):
+        takePicture()
+        br = brightness( file )
+        print ( f'brigtnness: {br}, current: {current_position}' )
+        rotateToPosition( br )
+        if readButtons() == [0,1,0,0]:
+            rotateToPosition(0)
+            return
+
+    
 
 try:
     while True:
         btns = readButtons()
         if btns == [1,0,0,0]:
-            rotate('forward1')
+#             rotate('forward1')
+            processImage()
         elif btns == [0,1,0,0]:
-            rotate('backward1')
+            break
+#             rotate('backward1')
         elif btns == [0,0,1,0]:
-            rotate('forward2')
+            rotateOneStep( -1 )
         elif btns == [0,0,0,1]:
-            rotate('backward2')
-        elif btns == [1,0,1,0]:
-            rotate('forward')
-        elif btns == [0,1,0,1]:
-            rotate('backward')
-        elif btns == [1, 1, 0, 0]:
-            takePicture()
+            rotateOneStep( 1 )
+#             rotate('backward2')
+#         elif btns == [1,0,1,0]:
+#             rotate('forward')
+#         elif btns == [0,1,0,1]:
+#             rotate('backward')
+#         elif btns == [1, 1, 0, 0]:
+#             takePicture()
+#             processImage()
         else:
             outputCommand(idle)
 except KeyboardInterrupt:
+    rotateToPosition( 0 )
     print(exit)
     GPIO.cleanup()
 
